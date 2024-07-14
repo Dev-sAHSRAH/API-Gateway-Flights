940b3cf341f0f65c07d368feab41728eb4712538
harsha <c.harsha0909@gmail.com>
2024-07-14 23:19:31 +0530

    added encryption in signup

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 "use strict";
 const { Model } = require("sequelize");
+const bcrypt = require("bcrypt");
+const { ServerConfig } = require("../config");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -34,5 +36,12 @@ module.exports = (sequelize, DataTypes) => {
       modelName: "User",
     }
   );
+  User.beforeCreate(function encrypt(user) {
+    const encryptedPassword = bcrypt.hashSync(
+      user.password,
+      parseInt(ServerConfig.SALT_ROUNDS)
+    );
+    user.password = encryptedPassword;
+  });
   return User;
 };
